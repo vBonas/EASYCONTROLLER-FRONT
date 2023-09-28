b7ff8678a7208472dce6f6e0f42148064ff71004
arielsam567 <Saidemim112104>
2023-09-28 16:39:03 -0300

    wip change url

--- src/app/time-controller-domain/time-controller-domain.service.ts
+++ src/app/time-controller-domain/time-controller-domain.service.ts
@@ -5,7 +5,7 @@ import { firstValueFrom } from 'rxjs';
 @Injectable()
 export class TimeControllerDomainService {
   constructor(private http: HttpClient) {}
-  url = 'http://almir.pythonanywhere.com/appview/easy-controller/';
+  url = 'https://almir.pythonanywhere.com/appview/easy-controller/';
   headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
@@ -29,18 +29,15 @@ export class TimeControllerDomainService {
 
   async calc_lqi(data: any) {
     return await firstValueFrom(
-      this.http.post(
-        'http://almir.pythonanywhere.com/appview/easy-controller/lqi',
-        data,
-        { headers: this.headers }
-      )
+      this.http.post(`${this.url}/lqi`, data, { headers: this.headers })
     );
   }
 
   async calc_lqg(data: any) {
     return await firstValueFrom(
       this.http.post(
-        'http://almir.pythonanywhere.com/appview/easy-controller/lqg',
+        `${this.url}/lqg`,
+        //'http://almir.pythonanywhere.com/appview/easy-controller/lqg',
         data,
         { headers: this.headers }
       )
@@ -50,7 +47,8 @@ export class TimeControllerDomainService {
   async calc_lqgi(data: any) {
     return await firstValueFrom(
       this.http.post(
-        'http://almir.pythonanywhere.com/appview/easy-controller/lqgi',
+        `${this.url}/lqgi`,
+        //'http://almir.pythonanywhere.com/appview/easy-controller/lqgi',
         data,
         { headers: this.headers }
       )
