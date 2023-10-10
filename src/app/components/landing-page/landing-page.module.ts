68dd2870cc3d61607492e8ae75a26c1c70681002
arielsam <arielsam@weg.net>
2023-10-10 10:12:54 -0300

    wip

--- src/app/components/landing-page/landing-page.module.ts
+++ src/app/components/landing-page/landing-page.module.ts
@@ -5,14 +5,14 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
-import { ClipboardModule } from '@angular/cdk/clipboard';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LandingPageComponent } from './landing-page.component';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
+//mat-toolbar
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [LandingPageComponent],
@@ -24,12 +24,11 @@ import { LandingPageRoutingModule } from './landing-page-routing.module';
     MatButtonModule,
     MatIconModule,
     MatCheckboxModule,
-    HttpClientModule,
-    ClipboardModule,
     MatCardModule,
     MatSidenavModule,
     MatSnackBarModule,
     LandingPageRoutingModule,
+    MatToolbarModule,
   ],
 
   providers: [],
